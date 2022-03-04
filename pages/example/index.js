946dec141a2c010ec53f6d8c2304b9534fd23279
nimoc <nimocc@126.com>
2022-03-04 10:24:41 +0800

    example index

--- pages/example/index.js
+++ pages/example/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import App from './index.vue';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import router from './router';
+import router from '@/router'
+import store from '@/store'
+
 // import store from './store';
 
 Vue.use(ElementUI)
 
 new Vue({
-    // router,
-    // store,
+    router,
+    store,
     render: h => h(App),
 }).$mount('#app');
