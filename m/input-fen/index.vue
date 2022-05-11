edcf46dce83c24d2612977cc3d9f9b40855a42f9
nimoc <nimocc@126.com>
2022-05-11 09:54:29 +0800

    input-fen

--- m/input-fen/index.vue
+++ m/input-fen/index.vue
@@ -1,10 +1,25 @@
 <template>
-    <el-input-number :value="value" @change="changeValue" :precision="2" :step="step" :max="max" :min="min" ></el-input-number>
+    <el-input-number
+    :value="parseFloat(parseFloat(value/100).toFixed(2))"
+    @change="(value) => changeValue(parseInt(parseFloat(value*100).toFixed(0)))"
+    :step="step"
+    :max="max"
+    :min="min"
+    :step-strictly="stepStrictly"
+    :size="size"
+    :disabled="disabled"
+    :controls="controls"
+    :controls-position="controlsPosition"
+    :name="name"
+    :label="label"
+    :placeholder="placeholder"
+    :precision="2"
+></el-input-number>
 </template>
 <script lang="js">
     export default {
         name: "InputFen",
-        props: ['value' ,'step', 'min', 'max'],
+        props:['value', 'min', 'max', 'step', 'step-strictly', 'precision', 'size', 'disabled', 'controls', 'controls-position', 'name', 'label', 'placeholder'],
         methods: {
             changeValue (v) {
                 this.$emit("input", v)
