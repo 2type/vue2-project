0e86971a448fe745f567e3bc71a3ab9274f7a3e5
Nimo Chu <3949015+nimoc@users.noreply.github.com>
2023-08-08 16:07:20 +0800

    x

---
+++ dist/assets/index-legacy.0715fed2.js
@@ -0,0 +1,11 @@
+;(function(){System.register(['./index-legacy.be3ee0bf.js','./vendor-legacy.44f98a50.js'],function(exports){'use strict';var normalizeComponent;return{setters:[function(module){normalizeComponent=module.n;},function(){}],execute:function execute(){var render=function render(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._v(" user "),_c('br'),_vm._v(" store.state.exampleCount is "),_c('el-tag',[_vm._v(" "+_vm._s(_vm.$store.state.exampleCount)+" ")])],1);};var staticRenderFns=[];//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+var __vue2_script={data:function data(){return{};}};var __cssModules={};var __component__=/*#__PURE__*/normalizeComponent(__vue2_script,render,staticRenderFns,false,__vue2_injectStyles,null,null,null);function __vue2_injectStyles(context){for(var o in __cssModules){this[o]=__cssModules[o];}}var index=exports('default',/*#__PURE__*/function(){return __component__.exports;}());}};});})();
